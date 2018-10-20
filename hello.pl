97edd639bddc2cbdf7b2e41577fb461e5030c6f6
Taekyoung Kim <johndoe@example.com>
2018-10-21 01:54:46 +0900

    a message

--- hello.pl
+++ hello.pl
@@ -2,7 +2,10 @@
 
 use Git::Class::Worktree;
 
-my $work = Git::Class::Worktree->new(path => 'test');
-$work->init;
+my $work = Git::Class::Worktree->new(path => 'workspace');
+$work->pull;
+$work->add('.');
+$work->commit({ message => 'a message' });
+$work->push;
 
 print "hello perl!";
